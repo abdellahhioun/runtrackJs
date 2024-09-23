345bc015e06a6f15a9e0d719a556aee155935343
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-23 14:32:37 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:3000/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        alert(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
